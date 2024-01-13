fd45a7aa0f4621981a5358913da6664716842753
Filip Milosevic <filip.milosevic29@gmail.com>
2024-01-13 16:53:53 +0100

    Revert changes for routes

--- library/backend/index.js
+++ library/backend/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import express, { Router, json } from "express";
+import express, { json } from "express";
 import { readFileSync } from "fs";
 import { join } from "path";
 import serveStatic from "serve-static";
@@ -26,16 +26,14 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-const router = new Router();
-
-router.post("/api/register", async (req, res) => {
+app.post("/api/register", async (req, res) => {
   const response = await axios.post(`${CENTRAL_URL} users`, req.body);
   res
     .status(response.status)
     .send(response.data);
 });
 
-router.post("/api/borrow", async (req, res) => {
+app.post("/api/borrow", async (req, res) => {
   const response = await axios.post(`${CENTRAL_URL} borrows`, req.body);
 
   if (response.data.error) {
@@ -58,16 +56,14 @@ router.post("/api/borrow", async (req, res) => {
     .send("Successfully borrowed!");
 });
 
-router.use(serveStatic(STATIC_PATH))
+app.use(serveStatic(STATIC_PATH))
 
-router.use("/*", async (_req, res, _next) => {
+app.use("/*", async (_req, res, _next) => {
   return res
     .status(200)
     .set("Content-Type", "text/html")
     .send(readFileSync(join(STATIC_PATH, "index.html")));
 });
-
-app.use(process.env.NODE_ENV === "prod" ? process.env.PATH : "/lib", router);
 
 app.listen(PORT, () => {
   console.log(`${LIBRARY} Library app is listening on ${PORT} `);
